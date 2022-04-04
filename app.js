322e7f1389053494c781abe5ee24e054e570267d
DaanKorver <daank2000@hotmail.nl>
2022-04-04 11:35:33 +0200

    :tada: initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 42069
+const server = app.listen(PORT, () => {
+  console.log(`App listening on http://[${server.address().address}]:${server.address().port}`)
+})
+
+const indexRoute = require('./routes/index')
+
+app.use('/', indexRoute)
